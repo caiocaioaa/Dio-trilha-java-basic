d04d5393dcd67b96a036e54db82e3919c39ea760
Caiop <caioalex2022@gmail.com>
2024-05-09 11:23:44 -0300

    mudança 2

--- ContaBanco/src/contaTerminal.java
+++ ContaBanco/src/contaTerminal.java
@@ -1,7 +1,37 @@
+
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO:Conhecer e importar a classe Scanner
 
-        //
+        //Exibir as mensagens para o nosso terminal
+
+        //Obter pela Scanner os valores digitados no terminal
+
+        //Exibir a mensagem criada
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da agência :");
+        numero = sc.nextInt();
+
+        System.out.println("Digite o nome da agencia :");
+        agencia = sc.next();
+
+        System.out.println("Digite o seu nome :");
+        nomeCliente = sc.next();
+
+        System.out.println("Digite o saldo da sua conta :");
+        saldo = sc.nextDouble();
+
+    System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta na agência bancária, a agência bancária é " + agencia + " conta " + numero + ", o saldo da conta é R$" + saldo);
+
+
     }
 }
